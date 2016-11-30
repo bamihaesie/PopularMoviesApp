0ae97c76ee57ad540d061038c1262ee75b0556db
Bogdan Amihaesie <bogami@amazon.com>
2016-11-30 11:15:58 -0800

    Add ButterKnife support

--- app/src/main/java/com/amazon/bogami/popularmoviesapp/activity/DetailActivity.java
+++ app/src/main/java/com/amazon/bogami/popularmoviesapp/activity/DetailActivity.java
@@ -12,33 +12,37 @@ import com.amazon.bogami.popularmoviesapp.R;
 import com.amazon.bogami.popularmoviesapp.model.Movie;
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.originalTitle) TextView titleView;
+    @BindView(R.id.releaseDate) TextView releaseDateView;
+    @BindView(R.id.userRating) TextView useRatingView;
+    @BindView(R.id.plotSynopsis) TextView plotSynopsisView;
+    @BindView(R.id.poster) ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
+        ButterKnife.bind(this);
         setTitle("Movie Detail");
         displayBackButton();
 
         Intent intent = getIntent();
         Movie selectedMovie = intent.getParcelableExtra("movie");
 
-        TextView titleView = (TextView) this.findViewById(R.id.originalTitle);
         titleView.setText(selectedMovie.getOriginalTitle());
 
-        TextView releaseDateView = (TextView) this.findViewById(R.id.releaseDate);
         releaseDateView.setText(selectedMovie.getReleaseDate());
 
-        TextView useRatingView = (TextView) this.findViewById(R.id.userRating);
         useRatingView.setText(getResources().getString(R.string.userRating,
                 Double.valueOf(selectedMovie.getUserRating()).toString()));
 
-        TextView plotSynopsisView = (TextView) this.findViewById(R.id.plotSynopsis);
         plotSynopsisView.setText(selectedMovie.getPlotSynopsis());
 
-        ImageView imageView = (ImageView) this.findViewById(R.id.poster);
         Picasso.with(getApplicationContext()).load("http://image.tmdb.org/t/p/w500/" + selectedMovie.getPosterPath()).into(imageView);
     }
 
